0c7bd67a97f265a3c210a02a7c9b00b98fe3f07d
jaf7 <janthonyfields@gmail.com>
2018-02-21 17:07:58 -0500

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
+require 'pry'
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    # binding.pry
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
